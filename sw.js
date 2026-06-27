99f00c66c25c8d62464b709abccbc5de6eb9f207
worldinovabr <worldinovabr@users.noreply.github.com>
2026-06-27 17:45:05 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'sensory-checkup-v1';
+const CACHE_NAME = 'sensory-checkup-v2';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
 ];
 
 // Install event - cache assets
